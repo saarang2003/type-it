902cfdd4f91889f77943c953ad4eb65db00b0b61
sarang <saranggp2018@gmail.com>
2025-05-24 18:15:13 +0530

    completed type safety

---
+++ app/(data)/types.ts
@@ -0,0 +1,8 @@
+import {data} from '../(data)/index';
+
+
+export type TypemodeType = keyof typeof data.typemode;
+export type TypemodeTime = (typeof data.typemode.time)[number];
+export type TypemodeWords = (typeof data.typemode.words)[number];
+export type ThemeType = (typeof data.theme)[number];
+export type CaretStyleType = (typeof data.caret)[number];
