9de9153f75622405aaed1905d0e8da8b3bbeec28
sarang <saranggp2018@gmail.com>
2025-05-25 13:44:09 +0530

    completed stylign theme

---
+++ app/components/Footer/page.tsx
@@ -0,0 +1,16 @@
+// import { TypingResult } from "@/app/types";
+
+
+
+
+// interface Props{
+//     rowCode : string | null;
+//     onPreviewResult : (result: TypingResult) => void; // Replace 'any' with the actual type of result
+// }
+
+export default function Footer(){
+
+    // const {roomCode , onPreviewResult} = props;
+
+    // const {typingFocused} = useContext(TypingContext);
+}
