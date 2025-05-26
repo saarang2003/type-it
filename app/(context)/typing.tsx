71ac68c46ce0c9f48272fc9ea9470f2771946cf8
sarang <saranggp2018@gmail.com>
2025-05-26 09:17:00 +0530

    done with qoute modal component

--- app/(context)/typing.tsx
+++ app/(context)/typing.tsx
@@ -1,7 +1,8 @@
-'use client';
+"use client";
 
-import { TypingResult } from "../types";
-
+import { createContext, useEffect, useState } from 'react';
+import { TypingResult } from '@/types';
+import { ResultOptions } from '@/components/Typing/Result';
 
 interface Context {
   typingStarted: boolean;
@@ -19,3 +20,89 @@ interface Context {
   onPreviewResult: (result: TypingResult | null, options?: ResultOptions) => void;
   onUpdateTypingFocus: (bool: boolean) => void;
 }
+const initial: Context = {
+  typingStarted: false,
+  typingFocused: false,
+  typingDisabled: false,
+  resultPreview: null,
+  lineHeight: 0,
+  typemodeVisible: true,
+  setTypemodeVisible: () => {},
+  setLineHeight: () => {},
+  onTypingStarted: () => {},
+  onTypingEnded: () => {},
+  onTypingDisable: () => {},
+  onTypingAllow: () => {},
+  onPreviewResult: () => {},
+  onUpdateTypingFocus: () => {},
+};
+
+const TypingContext = createContext(initial);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const TypingContextProvider = ({ children }: Props) => {
+  const [typingStarted, setTypingStarted] = useState(initial.typingStarted);
+  const [typingFocused, setTypingFocused] = useState(initial.typingFocused);
+  const [typingDisabled, setTypingDisabled] = useState(initial.typingDisabled);
+  const [resultPreview, setResultPreview] = useState(initial.resultPreview);
+  const [lineHeight, setLineHeight] = useState(initial.lineHeight);
+  const [typemodeVisible, setTypemodeVisible] = useState(initial.typemodeVisible);
+
+  const onTypingStarted = () => setTypingStarted(true);
+  const onTypingEnded = () => setTypingStarted(false);
+
+  const onTypingDisable = () => setTypingDisabled(true);
+  const onTypingAllow = () => setTypingDisabled(false);
+
+  const onPreviewResult: Context['onPreviewResult'] = (result, options) => {
+    setResultPreview(result ? { state: result, options } : null);
+  };
+
+  const onUpdateTypingFocus: Context['onUpdateTypingFocus'] = (bool) => {
+    setTypingFocused(bool);
+  };
+
+  useEffect(() => {
+    if (typingFocused) {
+      document.documentElement.style.cursor = 'none';
+    } else {
+      document.documentElement.style.cursor = 'auto';
+    }
+  }, [typingFocused]);
+
+  useEffect(() => {
+    if (resultPreview === null) {
+      setTypemodeVisible(true);
+    } else {
+      setTypemodeVisible(false);
+    }
+  }, [resultPreview]);
+
+  return (
+    <TypingContext.Provider
+      value={{
+        typingStarted,
+        typingFocused,
+        typingDisabled,
+        resultPreview,
+        lineHeight,
+        typemodeVisible,
+        setTypemodeVisible,
+        onTypingStarted,
+        onTypingEnded,
+        onTypingDisable,
+        onTypingAllow,
+        onPreviewResult,
+        onUpdateTypingFocus,
+        setLineHeight,
+      }}
+    >
+      {children}
+    </TypingContext.Provider>
+  );
+};
+
+export { TypingContext, TypingContextProvider };
---
+++ app/components/Typing/types.ts
@@ -0,0 +1,12 @@
+
+type TypingCharType = 'correct' | 'incorrect' | 'extra' | 'none';
+
+type TypingWords = {
+  isIncorrect: boolean;
+  chars: {
+    content: string;
+    type: TypingCharType;
+  }[];
+}[];
+
+export type { TypingCharType, TypingWords };
